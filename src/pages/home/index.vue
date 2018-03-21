897615f7bbfe0244af8d46426d95cc304c876508
hadoop <KenySee@126.com>
2018-03-21 16:58:14 +0800

    init

---
+++ src/pages/home/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="reveal">
+    <div class="slides">
+      <section>Slide 11</section>
+      <section>Slide 2</section>
+      <section>Slide 3</section>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    mounted(){
+      Reveal.initialize({
+        dependencies: [
+          { src: './static/reveal/plugin/markdown/marked.js' },
+          { src: './static/reveal/plugin/markdown/markdown.js' },
+          { src: './static/reveal/plugin/notes/notes.js', async: true },
+          { src: './static/reveal/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+        ]
+      });
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+  /*@import "HelloWorld.less";*/
+</style>
